555c0c75d661f987511c638b06d0ce46e603cd6c
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-01-12 00:02:37 -0600

    Se cambia color de fondo del header y tipografia

--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300&family=Dosis:wght@200&family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/main.css">
-	<title>Document</title>
+	<title>Habitos</title>
 </head>
 <body>
 	<header>
 		<h1>Seguimiento de habitos</h1>
 	</header>
+
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&display=swap" rel="stylesheet">
