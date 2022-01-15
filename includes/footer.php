84c480cf21cd67a3f482661d8903879b52d0b16f
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-01-14 18:43:29 -0600

    Se agrega footer con diseno y se incluye el section

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+		<footer>
+			<div>Desarrollado por Luis Enrique Jimenez &copy <?=date('Y');?> </div>
+			<div><a href="https://github.com/LuisEnriqueJT/habitos.git">Repositorio</a></div>
+		</footer>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php require_once 'includes/header.php';?>
+<?php require_once 'includes/section.php';?>
+<?php require_once 'includes/footer.php';?>
